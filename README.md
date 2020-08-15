c1b166c8f761f1894a8596cee6d9a59c3bf6443d
predator-stormbreaker <rishabh2583chauhan@gmail.com>
2020-08-15 10:20:40 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,105 @@
+<h1 align="center"> Predator Stormbreaker </h1> 
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/predator-stormbreaker/predator-stormbreaker.github.io/05c3f725035d41b9538120792bf565c998ece3bf/assets/img/Programming.svg"></img>
+</p>
+
+<h3 align="center"> A static website, Designed with bootstrap from scratch, Customizable Template <br/> Best Template for Android Kernel Developers Only! </h3>
+
+<p align="center">
+      <a href="https://travis-ci.org/badges/badgerbadgerbadger"><img alt="Build Status" src="http://img.shields.io/travis/badges/badgerbadgerbadger.svg?style=flat-square?style=flat-square" /></a>
+  <a href="http://badges.mit-license.org/"><img alt="License" src="http://img.shields.io/:license-mit-blue.svg?style=flat-square?style=flat-square" /></a>
+  <a href="https://github.com/ashutosh1919/masterPortfolio/commits/master"><img alt="Maintenance" src="https://img.shields.io/badge/maintained-yes-green.svg?style=flat-square" /></a>
+  <a href="http://badges.mit-license.org/"><img alt="Website" src="https://img.shields.io/badge/website-up-yellow?style=flat-square" /></a>
+  <a href=""><img alt="Uptime" src="https://img.shields.io/badge/uptime-100%25-brightgreen" ></a>
+  <a href="https://img.shields.io/badge/price-free-ff69b4"><img alt="Price" src="https://img.shields.io/badge/price-free-ff69b4?style=flat-square" /></a>
+</p>
+
+<p align="center"> 
+    <img src="https://raw.githubusercontent.com/predator-stormbreaker/predator-stormbreaker.github.io/master/assets/img/hero.png" target="_blank"></img>
+  </a>
+</p>
+
+
+# Pages Included 📝
+
+✔️ Home\
+✔️ Downloads \
+✔️ Github Source\
+✔️ Faqs\
+✔️ Feedback\
+✔️ About Us
+
+Check Out Our Website, **[click here](https://predator-stormbreaker.github.io/)**
+
+
+# Fork And Use 📋
+
+- It is a Static Website completely build from the Core.
+- We have used - Latest HTML, Css, Javascript, Bootstrap Libraries, Animations.
+- Thanks to Bootstrap Community for helping us whenever we got struck somewhere.
+- Fork this Repository, or Clone it locally & Finally edit Html and Push.
+
+
+# Make it your's ✏️
+
+We have used only Html, Css, Javascript instead of making it more complex using React, Nodejs or Npm. In this project, You need to edit/modify few files such as **Html files**, **Css** and **Javascript in some cases**. 
+
+
+
+### About Predator Stormbreaker
+
+Predator Stormbreaker is one of the Best Kernels available for Redmi Note 7 aka Lavender. If you own a Lavender, Must Flash Predator Stormbreaker Kernel once to Try something New & Fresh. 
+
+Developer - [PredatorX91](https://t.me/PredatorX91) <br>
+Co-Maintainer - [susandahal](https://t.me/susandahal)
+
+
+Our aim is to Provide a Stable Kernel with an Improved Experience! Our Kernel has tweaks which will make your performance good and will make your battery last longer. We provides Frequent Updates in order to ensure Stability.
+
+
+# Github Pages ✨
+
+- This Website is hosted upon Github Pages.
+- We highly recommend using [Github Pages](https://create-react-app.dev/docs/deployment/#github-pages) to achieve this the EASIEST WAY. If you want to develop a Website in no time, You can Fork our Website Source & Can Edit & Push.
+- To deploy your website, you have two options. First you need to create a github repository with the name `<your-github-username>.github.io`. Please don't give it any other name.
+- Now, you need to generate a production build and deploy the website.
+
+<p align="center"> 
+    <img src="https://raw.githubusercontent.com/predator-stormbreaker/predator-stormbreaker.github.io/master/assets/img/20200622_213732-01-01.jpeg"></img>
+</p>
+
+# Assets used from ⚡
+
+- [HTML](https://html.com/html5)
+- [Svg Images](https://www.freepik.com/)
+- [Css Modifications](https://css-tricks.com/)
+- [Background](https://webkul.github.io/coolhue/)
+- [Icons](https://boxicons.com/)
+- [Design Ideas](https://bootstrapmade.com/)
+- [Animations](https://www.gradient-animator.com/)
+
+
+# License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/predator-stormbreaker/predator-stormbreaker.github.io/blob/master/LICENSE) file for details.
+
+
+# Credits 💯 
+
+- Web Sources such as FreePik, for providing us Un-Copyrighted Images, Icons, Vectors. 
+- Bootstrap Community, For guiding us in every phase.
+- CssTricks.com, For revealing some best tricks which everyone should use in their Website.
+- Ashutosh Pro Sar, For README.md
+- Predator Gang, For Everything and Everything... 🤩
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/predator-stormbreaker/predator-stormbreaker.github.io/05c3f725035d41b9538120792bf565c998ece3bf/assets/img/www.svg"> </img>
+</p>
+
+# Contribute ✌🏻
+This Repository is Open Sourced. You can Fork, Create Pull Requests 🙂 with
+- Responsive Layout <br>
+- Little More Animations <br>
+
+ If you want to Contribute, Kindly create pull requests on the above mentioned concepts. Kindly give proper Credits to us 😇 
---
+++ assets/js/main.js
@@ -0,0 +1,219 @@
+!(function($) {
+  "use strict";
+
+  // Intro carousel
+  var introCarousel = $(".carousel");
+  var introCarouselIndicators = $(".carousel-indicators");
+  introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+  });
+
+  introCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
+  
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(20).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 30,
+      backDelay: 1100 
+    });
+  }
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+ 
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+  
+  
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+// Clients carousel (uses the Owl Carousel library)
+  $ (".clients-carousel").owlCarousel({
+    autoplay: true,   dots: true,   loop: true,
+    responsive: {
+      0: {    items: 2    },
+      768: {   items: 4    },
+        900: {   items: 6   }
+    }  });
+    
+ // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true, dots: true, loop: true, center: true, margin: 16,
+    responsive: {
+      0: {  items: 1  },
+      768: {    items: 3  },
+      992: {    items: 4   },
+      1200: {    items: 5   }  }  });
+      
+  $(document).ready(function(){
+  $(".gallery-carousel").owlCarousel(); });
+  
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+  
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+  
+})(jQuery); 
